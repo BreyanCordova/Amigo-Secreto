c6da045e576abdb12c5dac9227c01d117594f260
Breyan Cordova <breyancordova@gmail.com>
2025-01-24 18:01:55 -0500

    Funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){ //Botón Añadir: agrega nombres a la lista
+    const nombre = document.querySelector(`#amigo`).value.trim();//Obtiene y elimina espacios en blanco
+
+        if (nombre !== ""){//valida si el input esta en blanco
+            amigos.push(nombre);
+        }else {
+            alert("Por favor, inserte un nombre.");
+        }
+    document.querySelector(`#amigo`).value = ``;
+    console.log(amigos);
+    actualizarLista();
+
+}
+
